65b67039448a223a3adfec28366cd619710b2d48
unknown <pajakupus@gmail.com>
2020-12-09 17:20:21 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCWDO7FoocT4U5_PG6KXiH08PY5ZRSzrdc",
+    authDomain: "clientpanel-bf075.firebaseapp.com",
+    databaseURL: "https://clientpanel-bf075.firebaseio.com",
+    projectId: "clientpanel-bf075",
+    storageBucket: "clientpanel-bf075.appspot.com",
+    messagingSenderId: "360335658033",
+    appId: "1:360335658033:web:f6050ce5682c06b147d910"
+  }
 };
